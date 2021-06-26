a7a2253e1ee6a68a14a77463e1f48ef05773dd5a
akankshakriti09 <56928016+akankshakriti09@users.noreply.github.com>
2021-06-26 22:55:46 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>Software Engineering</h1>
+    </header>
+  );
+}
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Waterfall Model</h1>
+      <p>
+        The waterfall model, sometimes called the classic life cycle, suggests a
+        systematic sequential approach to software development that begins with
+        customer specification of requirements and progresses through planning,
+        modeling, construction, and deployment.
+      </p>
+    </div>
+  );
+}
+export default Note;
